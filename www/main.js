20830b2ab7aac101ce5b7c62d0e474819a7d4e84
milar111 <114105495+milar111@users.noreply.github.com>
2024-05-05 23:14:32 +0300

    Ai UI begining

---
+++ www/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: null
+    });
+
+    //siri configuration
+
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",  //can easely be switched to ios
+        speed: 0.07,
+        amplitude: 1,
+        autostart: true
+      });
+
+      //Siri message animation
+        $('.siri-message').textillate({
+            loop: true,
+            sync: true,
+            in: {
+                effect: "fadeInUp",
+                sync: true,
+            },
+            out: {
+                effect: "fadeOutUp",
+                sync: true,
+            }
+        });
+
+});
