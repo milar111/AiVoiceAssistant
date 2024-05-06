23bc87ceb444780bc1b771ae49684a1fa778f085
milar111 <114105495+milar111@users.noreply.github.com>
2024-05-06 11:29:10 +0300

    noSound
    Cant find the issue

--- engine/features.py
+++ engine/features.py
@@ -1,6 +1,6 @@
-from playsound import playsound
+# from playsound import playsound
 
-# # Play assistant sound
-# def playAssistantSound():
-music_dir = "www\\assets\\audio\\start_sound.mp3"
-playsound(music_dir)
+# # # Play assistant sound
+# # def playAssistantSound():
+# music_dir = "www\\assets\\audio\\start_sound.mp3"
+# playsound(music_dir)
